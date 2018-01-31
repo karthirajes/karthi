bb642393dd5d9fe51c4b8c786fa85c37b1a510e5
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-31 15:49:38 +0530

    Create two.c

---
+++ two.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int n,k,j,count=0,sum=0;
+scanf("%d",&n);
+k=n;
+while(k!=0){
+    k=k/10;
+    count++;
+}
+k=n;
+while(n!=0){
+    j=n%10;
+
+    printf("\n%d",j);
+    sum=sum+pow(j,count);
+    printf("\n%d",sum);
+    n=n/10;
+}
+if(k==sum)
+    printf("yes");
+else
+    printf("no");
+}
