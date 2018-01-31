dcceaec51be1c1bbfbe7d9c433071ec91b30c4c3
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-31 16:11:37 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+	int n,remainder,count=0,n1;
+	scanf("%d",&n);
+	n1=n;
+	while(n1!=0)
+	{
+		remainder=n1%10;
+		++count;
+		n1=n1/10;
+	}
+	printf("%d",count);
+getch();
+}
