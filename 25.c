95e0356e2c3c4d7c902c10dce3cfefc0df1d9c9d
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-31 15:56:08 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("\n Please Give The Value of N:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+     {
+        printf("\n 5 * %d = %d",i,5*i);
+     }
+     return 0;
+}
