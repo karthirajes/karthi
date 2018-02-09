23a23aeb16ab12670f6c08574ab7dab3e30de748
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-02-09 13:55:51 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[50];
+    int i,c=0;
+    printf("enter the string :");
+    scanf("%[^n]s",s1);
+    for(i=0;s1[i]!='\0';i++){
+if(s1[i]==' ')
+        {
+            c++;
+        }
+}
+ printf("no of words are :%d",c);
+}
+return 0;
+}
